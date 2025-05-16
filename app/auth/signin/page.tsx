6c50d8397eb1e3b7df59a2385a01f575af34edd0
skmayya1 <skandamayya4@gmail.com>
2025-05-16 16:09:33 +0530

    feat:auth page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,6 +3,7 @@ import { authClient } from '@/lib/auth-client';
 import React, { useEffect } from 'react'
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { FaGithub } from "react-icons/fa";
 
 
 const Page = () => {
@@ -19,8 +20,12 @@ const Page = () => {
   if (isPending || (session && session)) return null;
 
   return (
-      <div className='h-screen w-full'>
-          <button onClick={async () => {
+      <div className='h-screen w-full bg-[#0a0a0a] text-white flex items-center justify-center'>
+        <div className="h-[25%] w-[30%] flex items-center flex-col justify-center gap-10">
+          <h1 className='text-3xl'>Sign In to Tannva</h1>
+        <button
+        className='border flex items-center justify-center gap-5 text-zinc-400 rounded-3xl border-[#353535] p-4  text-xl tracking-wide w-[90%] hover:bg-[#252525] transition-all duration-150 ease-in-out'
+        onClick={async () => {
               toast.promise(
                   async () => { 
                   await authClient.signIn.social({
@@ -33,8 +38,10 @@ const Page = () => {
                     error: "Something went wrong"
                  })
           }}>
-              Sign In With Github 
+            <FaGithub size={22}/>  Sign in with github 
           </button>
+          <p className='text-xs ffonnt-thin text-zinc-600'>By continuing, you agree to Tannva's Terms of Service, Privacy Policy and Data Privacy Agreement</p>
+        </div>
     </div>
   ) 
 }
